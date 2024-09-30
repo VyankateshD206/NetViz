a15aa3559e885309e939c396a367facaf46b2313
= <=>
2024-10-01 00:41:27 +0530

    Starter folders and header files

---
+++ include/Device.h
@@ -0,0 +1,17 @@
+#ifndef DEVICE_H
+#define DEVICE_H
+
+#include<string>
+
+class device{
+    public:
+        std::string type;
+        std::string ip;
+
+        //default constructor
+        device(){
+            
+        }
+};
+
+#endif
---
+++ include/Network.h
@@ -0,0 +1,9 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+class Network{
+    protected:
+
+};
+
+#endif
